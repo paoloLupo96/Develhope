b75ee51f8b1ad9f1c68ebaa49119bd132359e1c3
Paolo <lupo.paolo96@gmail.com>
2023-11-22 10:41:19 +0100

    Added exercise3

---
+++ template-strings/exercise3/assignment.md
@@ -0,0 +1,1 @@
+-Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
---
+++ template-strings/exercise3/exercise.js
@@ -0,0 +1,5 @@
+const name = "John";
+const message = "Welcome to our website!";
+const greeting = `Hello, ${name}! ${message} `
+
+console.log(greeting);
