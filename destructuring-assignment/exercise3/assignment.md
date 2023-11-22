b76f8285295afcc22fe4e68d957cb89f986bb6dc
Paolo <lupo.paolo96@gmail.com>
2023-11-22 11:10:35 +0100

    Added exercise3

---
+++ destructuring-assignment/exercise3/assignment.md
@@ -0,0 +1,1 @@
+Dato un array di colori con tre elementi, utilizza la destrutturazione dell'array per creare variabili firstColor, secondColor e thirdColor che contengono i valori dell'array.
---
+++ destructuring-assignment/exercise3/exercise.js
@@ -0,0 +1,7 @@
+const colors = ["red", "green", "blue"];
+
+let [firstColor, secondColor, thirdColor] = colors;
+
+console.log(firstColor);
+console.log(secondColor);
+console.log(thirdColor);
