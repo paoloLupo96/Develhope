c806f202d292187d388d10432d75445a0a1a0186
Paolo <lupo.paolo96@gmail.com>
2023-11-22 10:31:58 +0100

    Added exercise2

---
+++ template-strings/exercise2/assignment.md
@@ -0,0 +1,1 @@
+-Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
---
+++ template-strings/exercise2/exercise.js
@@ -0,0 +1,6 @@
+let myQuote = `“Io ne ho viste cose...
+che voi umani non potreste immaginarvi”.
+
+-Sviluppatore Senior in burnout.`
+
+console.log(myQuote);
