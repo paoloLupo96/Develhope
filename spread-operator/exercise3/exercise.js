9d331d75ed50ecf2b49fc1866c973c9ce5442405
Paolo <lupo.paolo96@gmail.com>
2023-11-23 16:51:11 +0100

    Added exercise3

--- spread-operator/exercise3/exercise.js
+++ spread-operator/exercise3/exercise.js
@@ -1,1 +1,12 @@
 const originalArray = [1, 2, 3, 4, 5];
+const cloneArray = [...originalArray];
+
+console.log("Orginal array:", originalArray);
+console.log("Clone array:", cloneArray);
+
+//Dimostrazione dell'indipendenza dei due array:
+
+originalArray.push(6);
+
+console.log("Original after push:", originalArray);
+console.log("Clone after push:", cloneArray);
