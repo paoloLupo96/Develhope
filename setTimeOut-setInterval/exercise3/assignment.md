ae93d4e2f19de34b7413d225d9bdfceba069e3b7
Paolo <lupo.paolo96@gmail.com>
2023-11-23 17:46:48 +0100

    Added exercise3

---
+++ setTimeOut-setInterval/exercise3/assignment.md
@@ -0,0 +1,2 @@
+1. Utilizza setTimeout per chiamare la funzione "mostraPromemoria" dopo un ritardo di 5 secondi.
+2. Prima che scadano i 5 secondi stampa in console: "Il promemoria è in fase di impostazione".
---
+++ setTimeOut-setInterval/exercise3/exercise.js
@@ -0,0 +1,7 @@
+function mostraPromemoria() {
+    console.log("Promemoria: È ora di fare una pausa!");
+  }
+
+  console.log("Il promemoria è in fase d'impostazione...");
+  setTimeout(mostraPromemoria, 5000);
+  
