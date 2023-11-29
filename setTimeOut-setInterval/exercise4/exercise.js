8a04fbf23c6cdae7324787ca7af643836dc90fdb
Paolo <lupo.paolo96@gmail.com>
2023-11-29 17:32:07 +0100

    Modified ex4 into setTimeOut-setInterval folder

--- setTimeOut-setInterval/exercise4/exercise.js
+++ setTimeOut-setInterval/exercise4/exercise.js
@@ -1,9 +1,15 @@
-function lanciaDadi(dado1, dado2){
-   console.log(`Dado 1 = ${dado1}, Dado 2 = ${dado2}`);
-}
- 
-console.log("Lancio dei dadi in corso...");
 
-setTimeout(()=>{
-lanciaDadi(4, 6);
-}, 2000)
+function lancio() {
+   return Math.floor(Math.random() * 6) + 1;
+}
+
+function lanciaDadi() {
+   console.log("Lancio dei dadi in corso...");
+   let dado1 = lancio();
+   let dado2 = lancio();
+   setTimeout(() => { 
+      console.log(`Dado 1: ${dado1}, Dado 2: ${dado2}`) 
+   }, 2000);
+}
+
+lanciaDadi();
