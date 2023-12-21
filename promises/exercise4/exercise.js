23f33cd59a8550abcbebebfc988d2bc9db1d2e01
Paolo <lupo.paolo96@gmail.com>
2023-12-21 16:16:26 +0100

    Updated ex4 into promises folder

--- promises/exercise4/exercise.js
+++ promises/exercise4/exercise.js
@@ -1,13 +1,12 @@
 function fetchDataFromAPI() {
   return new Promise((resolve, reject) => {
-    let randomNumb = console.log(Math.random() * 0.6);
-
+    let randomNumb = Math.random() * 0.6;
     if (randomNumb < 0.5) {
       resolve("Data retrieved successfully");
-    } else reject("Error: Failed to fetch data");
+    } else reject(new Error("Error: Failed to fetch data"));
   });
 }
 
 fetchDataFromAPI()
   .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+  .catch((error) => console.log(error.message));
